7d4d7bd125292aa5324f4fe09cf263c4dfb8ccb9
Eslavita <santiagoeslava906@gmail.com>
2026-02-26 12:39:19 -0500

    agregado de archivos

--- src/users/dtos/user.dto.ts
+++ src/users/dtos/user.dto.ts
@@ -33,5 +33,10 @@ export class CreateUserDto {
     @IsNotEmpty()
     @ApiProperty()
     readonly miTest2: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty()
+    readonly miTest3: string;
 }
 export class UpdateUserDto extends PartialType(CreateUserDto){}
